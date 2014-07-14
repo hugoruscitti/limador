b0f7e4858cbd28bc8b70b369763794d813ef1800
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-07-14 00:23:20 -0300

    Colocando un ícono en el panel y haciendo que los fondos se puedan cambiar.

--- src/js/controllers/MainCtrl.js
+++ src/js/controllers/MainCtrl.js
@@ -4,9 +4,24 @@ var app = angular.module('app');
 app.controller("AppCtrl", function($scope) {
   $scope.data = {};
   $scope.data.panel_visible = false;
+  $scope.data.fondos = [
+    {
+      miniatura: './backgrounds/test.png',
+      video: './backgrounds/test.mov'
+    },
+    {
+      miniatura: './backgrounds/otro.png',
+      video: './backgrounds/otro.mov'
+    },
+  ];
 
   $scope.mostrar_panel = function() {
     $scope.data.panel_visible = !$scope.data.panel_visible;
   }
 
+  $scope.seleccionar = function(obj) {
+    $scope.data.video_seleccionado = obj.video;
+    var video = document.getElementById('video');
+    video.src = obj.video;
+  }
 });
